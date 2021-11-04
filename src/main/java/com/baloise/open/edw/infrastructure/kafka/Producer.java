ffa595f3ab82e44da6c5c2d107f2e8c6c7ff6a20
Arno Burkhart <arburk@users.noreply.github.com>
2021-11-04 09:34:16 +0100

    Introduced Producer interface

--- src/main/java/com/baloise/open/edw/infrastructure/kafka/Producer.java
+++ src/main/java/com/baloise/open/edw/infrastructure/kafka/Producer.java
@@ -1,63 +1,19 @@
 package com.baloise.open.edw.infrastructure.kafka;
 
-import com.baloise.open.edw.domain.kafka.Status;
-import com.baloise.open.edw.domain.services.CorrelationId;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
-import java.util.Collections;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-@Slf4j
-public class Producer extends Config {
+public interface Producer {
 
-  public Producer(Properties configProps, String topic, String clientId) throws ExecutionException, InterruptedException {
-    super(configProps, topic, clientId);
-    initTopic(Admin.create(getConfigProps()));
+  static Producer create(Properties configProps, String topic, String clientId) throws ExecutionException, InterruptedException {
+    return new ProducerImpl(configProps, topic, clientId);
   }
 
-  private void initTopic(final Admin admin) throws ExecutionException, InterruptedException {
-    isCreateMissingTopic(admin, STATUS_TOPIC_NAME);
-    registerProducer(isCreateMissingTopic(admin, getTopic()));
-  }
-
-  private void registerProducer(boolean isNewTopic) {
-    if (isNewTopic) {
-      Status status = new Status(getClientId(), getTopic(), Status.EventType.TOPIC_CREATED);
-      pushEvent(STATUS_TOPIC_NAME, CorrelationId.get(), status.toJson());
-    }
-
-    Status status = new Status(getClientId(), getTopic(), Status.EventType.CONNECT);
-    pushEvent(STATUS_TOPIC_NAME, CorrelationId.get(), status.toJson());
-  }
-
-  private boolean isCreateMissingTopic(Admin admin, String topicName) throws ExecutionException, InterruptedException {
-    final Set<String> names = admin.listTopics().names().get();
-    final boolean isTopicInexistent = names.stream().noneMatch(name -> name.equals(topicName));
-
-    if (!isTopicInexistent) {
-      log.debug("Topic '{}' exists, skip creation.", topicName);
-      return false;
-    }
-
-    final NewTopic newTopic = new NewTopic(topicName, 1, (short) 1);
-    admin.createTopics(Collections.singleton(newTopic)).values().get(topicName).get();
-    log.info("Created topic '{}'", topicName);
-    return true;
-  }
-
-  public Future<RecordMetadata> pushEvent(String topic, String key, Object value) {
-    final ProducerRecord<String, Object> producerRecord = new ProducerRecord<>(topic, key, value);
-    try (final KafkaProducer<String, Object> producer = new KafkaProducer<>(getConfigProps())) {
-      return producer.send(producerRecord);
-    }
-  }
-
+  /**
+   * Pushes an event to the given topic
+   */
+  Future<RecordMetadata> pushEvent(String topic, String key, Object value);
 }
